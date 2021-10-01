ece32e641986c690ea585289dc07cb46d896b426
Your Name <you@example.com>
2021-10-01 10:18:58 +0200

    memories

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Grid,CircularProgress} from '@material-ui/core';
+import {useSelector} from 'react-redux';
+import Post from './Post/Post.js';
+import useStyle from './styles';
+
+const Posts=({setCurrentId})=>{
+    const posts=useSelector((state)=>state.posts);
+    const classes=useStyle();
+
+    return(
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                {posts.map((post)=>(
+                    <Grid key={post._id} xs={12} sm={6} item>
+                        <Post post={post} setCurrentId={setCurrentId}/>
+                    </Grid>
+                ))}
+            </Grid>
+        )
+    );
+}
+
+export default Posts;
